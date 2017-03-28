69ae6207668a49edee229251e881158773c1a66d
Quinn Stephens <quinn@quinnstephens.com>
2017-03-28 14:47:19 -0500

    Separated home component

---
+++ Home.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react'
+import {
+  AppRegistry,
+  NativeModules,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native'
+
+async function getPermission() {
+  try {
+    let result = await NativeModules.SpeechManager.getPermission()
+    console.log(result)
+  } catch(e) {
+    console.error(e)
+  }
+}
+
+getPermission()
+
+export default class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    
+    this.toggleRecording = this.toggleRecording.bind(this)
+    this.speak = this.speak.bind(this)
+
+    this.state = { 
+      intro: "Talk to me like the rain and let me listen.",
+      recording: false,
+      result: ""
+    }
+
+    this.speak(this.state.intro)
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.instructions}>
+          {this.state.intro}
+        </Text>
+
+        <Text style={styles.result}>
+          {this.state.result}
+        </Text>
+
+        <TouchableHighlight style={this.state.recording ? styles.buttonActive : styles.button} onPress={this.toggleRecording}>
+          <Text style={styles.buttonText}>
+            {this.state.recording ? "Stop" : "Record"}
+          </Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+
+  async toggleRecording() {
+    this.setState({
+      recording: !this.state.recording  
+    })
+    try{
+      let result = await NativeModules.SpeechManager.toggleRecording()
+      this.setState({
+        result: result
+      })
+      this.speak(result)
+
+    } catch(e) {
+      console.error(e)
+    }
+  }
+
+  speak(text) {
+    NativeModules.SpeechManager.speak(text)
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#dddddd',
+    marginBottom: 5,
+  },
+  button: {
+    backgroundColor: 'green',
+    borderRadius: 8,
+    margin: 12,
+    padding: 24,
+  },
+  buttonActive: {
+    backgroundColor: 'red',
+    borderRadius: 8,
+    margin: 12,
+    padding: 24,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 22,
+  },
+  result: {
+    fontSize: 22,
+    padding: 24,
+  },
+})
